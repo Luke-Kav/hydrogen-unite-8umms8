49ed7d6a157ce6e72105d8997d80d667dcfe3f01
Luke-Kav <42727293+Luke-Kav@users.noreply.github.com>
2022-09-05 17:52:13 +1200

    Fin query for A Component

--- src/routes/products/[handle].server.jsx
+++ src/routes/products/[handle].server.jsx
@@ -1,12 +1,30 @@
 // /src/routes/products/example.server.jsx
 
+import { useShopQuery, gql } from '@shopify/hydrogen';
 import Layout from '../../components/Layout.server';
 
 export default function Example({ params }) {
   const { handle } = params;
+  const { data } = useShopQuery({
+    query: QUERY,
+    variables: {
+      handle,
+    },
+  });
+  console.log(data);
+
   return (
     <Layout>
       <p>Hello World! This is the product page for {handle}</p>
     </Layout>
   );
 }
+
+const QUERY = gql`
+ query productQuery($handle: String!) {
+   product(handle: $handle) {
+     id
+     title
+   }
+ }
+ `;
